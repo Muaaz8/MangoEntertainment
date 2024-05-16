d4da1667da791843b9379e78693ac609d15c08a2
Fahim-kh <111640774+Muaaz8@users.noreply.github.com>
2024-05-16 21:38:27 +0500

    liveStreaming

--- resources/views/Video/index.blade.php
+++ resources/views/Video/index.blade.php
@@ -32,6 +32,10 @@
         }
     </style>
     <script src="{{ asset('ZegoExpressWebRTC-3.4.0.js') }}"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -40,7 +44,12 @@
     </h1>
     <h4>Local video</h4>
     <div id="local-video"></div>
+    <button class="btn btn-primary" id="screenShareButton" onclick="screenShare()"> Screen Share </button>
+    <button class="btn btn-primary d-none" id="stopScreenShareButton" onclick="stopScreenShare()"> Stop Screen Share </button>
+    <button class="btn btn-primary" onclick="camera()"> Camera </button>
+    <button class="btn btn-primary" onclick="microphone()"> Microphone </button>
     <h4>Remote video</h4>
+    <button class="btn btn-primary" onclick="audio()"> Audio </button>
     <div id="remote-video"></div>
 </body>
 
@@ -50,21 +59,37 @@
     const roomID = "6644de0a19595";
     const userID = "cFR2dMKDtw";
     const userName = "test";
-    const token = "04AAAAAGZGKggAEGt1ZWJwZzMwYnY3ZWFkcTMAsCWR4DNSMop0IgZ+bLmxhQDv8ajSdc1tdcdvClz2XZjYZPZmgXKwhrv70xwq+ooemEaoVSa6ydZO4GUOwovmCqNdl68PDZ7EiQvNU71BR9HZz48hd9Ax8EcIKjJK4DVJ81W+YTD+3cdQChbDeI7LUe1iJD+jYzwelXhxVmm5ikN01m0ZH/fVl9SnMnYgb41dpx8djJSOWutLB2bYOM6SCrSfLin3ap2ow3yrY2yghhjD";
+    const token = "04AAAAAGZHfJEAEHZyenJvb2NtMzk2Y3dibHQAsPRINNa7UhmIxePYImG5uFu1VwN5u2j8L9aKGCcKmAMWDvIcgkXDEVjqheO3tQdwVcqFboUnN5OrYgKyzhzBM82Zib7/pDD8OKGMR79I26Eidj+be/u8oYMfWD48Rn7RZDK/1eOM8FZWjQrHP06KgnGWk/fG0zd6WIxfWHCBHfUPXvPsKFPIpg24gvjjic3/fdXGWAPrCl0Jp+1DsE3CiOd0uX9Vlf+JrWUX43dNjgj3";
     const streamID = '{{uniqid()}}';
+    var localStream;
+    var screenStreamId;
+    var screenStream;
+    let option = {
+        videoBitrate: 300,
+        audioBitrate: 48,
+            camera: {
+            audio: true,
+            video : {
+                quality: 4,
+                width: 480,
+                height: 480,
+                frameRate: 15
+            }
+        }
+    }
 
     const zg = new ZegoExpressEngine(appId, server);
 
     async function initZego() {
         const result = await zg.loginRoom(roomID, token, {userID, userName}, {userUpdate: true});
-        const localStream = await zg.createZegoStream();
+        localStream = await zg.createZegoStream(option);
         localStream.playVideo(document.querySelector("#local-video"));
         // localStream is the MediaStream object created by calling creatStream in the previous step.
         zg.startPublishingStream(streamID, localStream)
 
-        const remoteStream = await zg.startPlayingStream(streamID);
-        // The remote-video is the <div> element's id on your webpage.
-        remoteView.play("remote-video", {enableAutoplayDialog:true});
+        // const remoteStream = await zg.startPlayingStream(streamID);
+        // // The remote-video is the <div> element's id on your webpage.
+        // remoteView.play("remote-video", {enableAutoplayDialog:true});
     }
 
     initZego();
@@ -117,6 +142,60 @@
     zg.on('playQualityUpdate', (streamID,stats) => {
         // Callback for reporting stream playing quality.
     })
+
+    async function screenShare(){
+        document.querySelector('#remote-video').innerHTML = '';
+        // document.querySelector('#screenShareButton')
+        // document.querySelector('#stopScreenShareButton')
+        screenStream = await zg.createZegoStream({
+            screen: {
+                videoBitrate: 1500,
+                audio: true,
+                video: {
+                    quality: 5,
+                    frameRate: 30,
+                    width: 25,
+                    height: 25
+                }
+            },
+        });
+        screenStreamId = screenStream.zegoStream.Ie[0].id;
+        document.querySelector('#screenShareButton').classList.add('d-none');
+        document.querySelector('#stopScreenShareButton').classList.remove('d-none');
+        screenStream.playVideo(document.querySelector("#remote-video"));
+        const publisRes = zg.startPublishingStream(screenStreamId, screenStream);
+    }
+
+    function stopScreenShare(){
+        zg.stopPublishingStream(screenStreamId);
+        document.querySelector('#screenShareButton').classList.remove('d-none');
+        document.querySelector('#stopScreenShareButton').classList.add('d-none');
+        zg.destroyStream(screenStream);
+    }
+
+    function camera(){
+        if(localStream.zegoStream.originVideoTrack.enabled){
+            localStream.zegoStream.originVideoTrack.enabled = false;
+        }else{
+            localStream.zegoStream.originVideoTrack.enabled = true;
+        }
+    }
+    function microphone(){
+        if(localStream.zegoStream.originAudioTrack.enabled){
+            localStream.zegoStream.originAudioTrack.enabled = false;
+        }else{
+            localStream.zegoStream.originAudioTrack.enabled = true;
+        }
+    }
+    function audio(){
+        if(screenStream.zegoStream.originAudioTrack.enabled){
+            screenStream.zegoStream.originAudioTrack.enabled = false;
+        }else{
+            screenStream.zegoStream.originAudioTrack.enabled = true;
+        }
+    }
+
+
 
 </script>
 
